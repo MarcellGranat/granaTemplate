f553ac07246c34d30af95e7a2d35357fcb7e8c4f
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-08-22 23:20:31 +0200

    add man to logo

--- R/logo.R
+++ R/logo.R
@@ -1,4 +1,9 @@
+#' Insert logo from the package files
+#'
+#' @param x file name of the logo (without `.png`)
+#' @return Exact location of the logo.
+
+
 logo <- function(x = "mnb_intezet") {
-  
   system.file("logo", paste0(x, ".png"), package = "granaTemplate")
 }
